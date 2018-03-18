4b91da85f114c47407b08f8c18f8ed86b9997e6b
JeffYang <JeffYang@boyu66.net>
2018-03-18 17:39:30 +0800

    加入CORS，可接受外部請求及設計API建議事項

--- WebAPIDemo/App_Start/WebApiConfig.cs
+++ WebAPIDemo/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebAPIDemo
 {
@@ -9,6 +10,16 @@ namespace WebAPIDemo
     {
         public static void Register(HttpConfiguration config)
         {
+            config.EnableCors();
+            // 注意，domin後不能有斜線
+            var cors = new EnableCorsAttribute(origins: "http://localhost:25536", headers: "*", methods: "*"); config.EnableCors(cors);
+
+            /*DELETE動詞的重要性
+              AJAX在不同源政策之下，預設只支持GET、POST
+              而DELETE、PUT等其他動詞，會先送出Options，並且回傳該Server是否接受請求，若是接受才會正式送出。
+              因此，若是在API設計上有區分動詞，API的結構會較為安全，因為GET、POST以外的動詞會需要伺服器授權才能執行
+             */
+
             // Web API 設定和服務
 
             // Web API 屬性路由
